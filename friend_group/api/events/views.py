578abfed290c11fca22ec7fb85c83da56e87e878
vantage-ola <vantageola4@gmail.com>
2025-01-15 16:11:27 +0100

    +views

--- friend_group/api/events/views.py
+++ friend_group/api/events/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Event, EventRegistration, MockPayment
+from .serializers import EventSerializer, EventRegistrationSerializer, MockPaymentSerializer
 
-# Create your views here.
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+
+class EventRegistrationViewSet(viewsets.ModelViewSet):
+    queryset = EventRegistration.objects.all()
+    serializer_class = EventRegistrationSerializer
+    permission_classes = [IsAuthenticated]
+
+class MockPaymentViewSet(viewsets.ModelViewSet):
+    queryset = MockPayment.objects.all()
+    serializer_class = MockPaymentSerializer
+    permission_classes = [IsAuthenticated]
