83b635f739ecffe940004f20dfe73848925fe46c
vantage-ola <vantageola4@gmail.com>
2025-01-15 16:12:07 +0100

    url routing for the views

--- friend_group/api/config/urls.py
+++ friend_group/api/config/urls.py
@@ -1,22 +1,26 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from users.views import UserViewSet
+from speakers.views import SpeakerProfileViewSet, SpeakerAvailabilityViewSet
+from groups.views import GroupViewSet, GroupMembershipViewSet
+from events.views import EventViewSet, EventRegistrationViewSet, MockPaymentViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'speakers', SpeakerProfileViewSet)
+router.register(r'speaker-availability', SpeakerAvailabilityViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'group-memberships', GroupMembershipViewSet)
+router.register(r'events', EventViewSet)
+router.register(r'event-registrations', EventRegistrationViewSet)
+router.register(r'payments', MockPaymentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
