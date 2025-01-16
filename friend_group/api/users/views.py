34618fd03ce0dfda1c1966b87b4fa108d19440e0
vantage-ola <vantageola4@gmail.com>
2025-01-16 03:19:23 +0100

    +user signup view

--- friend_group/api/users/views.py
+++ friend_group/api/users/views.py
@@ -1,9 +1,33 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+# users/views.py
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == 'create':
+            # Allow anyone to register
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Create user with password
+        user = User.objects.create_user(
+            username=serializer.validated_data['username'],
+            email=serializer.validated_data['email'],
+            password=request.data['password'],  # Get password from request data
+            is_speaker=serializer.validated_data.get('is_speaker', False)
+        )
+
+        return Response(
+            UserSerializer(user).data,
+            status=status.HTTP_201_CREATED
+        )
