933c4b5f214275351a584a4235d0f9c3b8212eb6
vantage-ola <vantageola4@gmail.com>
2025-01-18 09:07:42 +0100

    add theme to main.tsx

--- friend_group/web/src/main.tsx
+++ friend_group/web/src/main.tsx
@@ -2,8 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import { CssBaseline } from '@mui/material'
+import AppTheme from './theme/AppTheme';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <AppTheme>
+    <CssBaseline enableColorScheme />
       <App />
+    </AppTheme>
   </StrictMode>,
 )
