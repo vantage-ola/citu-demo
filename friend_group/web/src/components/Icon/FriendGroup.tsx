4b58584d07ccf37440787f87091cbaa2d324623c
vantage-ola <vantageola4@gmail.com>
2025-01-27 22:33:44 +0100

    hotfix

---
+++ friend_group/web/src/components/Icon/FriendGroup.tsx
@@ -0,0 +1,18 @@
+import SvgIcon from '@mui/material/SvgIcon';
+
+export const FriendGroupIcon = () => {
+  return (
+    <SvgIcon sx={{ height: 21, width: 100, mr: 2 }} viewBox="0 0 200 40">
+      <text
+        x="10"
+        y="28"
+        fontFamily="Arial, sans-serif"
+        fontSize="24"
+        fontWeight="bold"
+        color='#027af2'
+      >
+        FriendGroup
+      </text>
+    </SvgIcon>
+  );
+}
